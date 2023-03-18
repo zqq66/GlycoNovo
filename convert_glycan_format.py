2893bf58c9752d47e93c8ea508edd8b82066869b
Qianqiu Zhang <q2zhang@bioinformaticssolutions.com>
2023-03-18 10:37:08 -0400

    convert StrucGP format to GlycoCT

---
+++ convert_glycan_format.py
@@ -0,0 +1,59 @@
+import glypy
+import csv
+import json
+mono_names = ['Man', 'GlcNAc', 'NeuAc', 'NeuGc', 'Fuc']
+
+
+def convert2glycoCT(structure_encoding):
+    idx = 0
+    for s in structure_encoding:
+        if s.islower():
+            structure_encoding = structure_encoding.replace(s, ']')
+        elif s.isupper():
+            structure_encoding = structure_encoding.replace(s, '[')
+
+    for s in structure_encoding[1:]:
+        if s == '[':
+            temp_lst = list(structure_encoding)
+            temp_lst.insert(idx + 1, ',')
+            idx += 2
+            structure_encoding = "".join(temp_lst)
+        else:
+            idx += 1
+    struc_lst = json.loads(structure_encoding)
+
+    root = mono_names[struc_lst[0] - 1]
+    glycan = glypy.Glycan(root=glypy.monosaccharides[root])
+    glycan = construct_glycan(glycan.root, glycan, struc_lst[1:], 0)
+    return glycan
+
+
+def construct_glycan(root, glycan, struc_lst, cur_idx):
+
+    for i, s in enumerate(struc_lst):
+
+        mono = glypy.monosaccharides[mono_names[s[0]-1]]
+        root.add_monosaccharide(mono)
+        next_idx = cur_idx+ 1
+        glycan.reindex(method='dfs')
+        root2 = glycan[next_idx]
+        construct_glycan(root2, glycan, s[1:], next_idx)
+    return glycan
+
+def convert_format():
+    strucgp_file = 'mouse_tissue_strucgp_result.csv'
+    new_file = 'mouse_tissue_strucgp_result_glycoct.csv'
+    with open(strucgp_file, 'r') as csvfile:
+        with open(new_file, 'w+') as dbfile:
+            csvreader = csv.DictReader(csvfile)
+            csvwriter = csv.writer(dbfile, delimiter=',')
+            for row in csvreader:
+                glycan_encoding = row['Structure_codinga']
+                glycoct = convert2glycoCT(glycan_encoding)
+                row['GlycoCT'] = glypy.io.glycoct.dumps(glycoct).replace('\n', ' ')
+                csvwriter.writerow([v for v in row.values()])
+
+                
+if __name__ == '__main__':
+    convert_format()
+
